daa32a930d7c65568ba0823445355d6bc71eaf0a
BooMBoTT workPC <AMalyj@roskazna.ru>
2024-08-13 11:36:13 +0300

    Телеграм бот доделан ответ с QRRAW

--- Finance_module/Finance_controller.py
+++ Finance_module/Finance_controller.py
@@ -1,13 +1,21 @@
+import FNS_parser
 
-def check_handler_QR(self, QR_link: str, user_id: str, type_id: int):
+def check_handler_QR(self, request: str, user_id: str, type_id: int):
     '''
     Достает по запросу данные чека.
     Если правильно, сохраняет данные.
     Выдает эти данные для интерфейса.
 
-    :param QR_link: str - Вводные данные, ссылка на QR код
+    :param request: str - Вводные данные, ссылка на QR код
     :param user_id: str - Айди пользователя, который отправил запрос
     :param type_id: int - Тип запроса, 1 QRRAW, 2 QR код
     :return:
     '''
-    pass
+    response = FNS_parser.get_data(request, type_id)
+    if response[0] == -1:
+        return (response[0], response[1])
+
+    organisation_name, total_sum, DT, products = response
+    print(organisation_name, total_sum, DT, products)
+    #TODO: Сюда вставить сохранение в базу данных
+    return organisation_name, total_sum, DT, products
--- telegram_interface.py
+++ telegram_interface.py
@@ -2,6 +2,7 @@ import Finance_module
 import telebot
 import dotenv
 import Finance_module
+from Classes import product
 '''Телеграмм бот'''
 from dotenv import load_dotenv
 import os
@@ -11,7 +12,12 @@ load_dotenv()
 TOKEN_TELEGRAMBOT = os.getenv('TOKEN_TELEGRAMBOT')
 bot = telebot.TeleBot(TOKEN_TELEGRAMBOT)
 
-
+#TODO: ниже
+'''
+QRRAW handler
+QR handler
+get_excel_file - достать эксельку
+'''
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -26,7 +32,12 @@ def text_handler(message):
 	bot.reply_to(message, message.text)
 	if message.text[0:1] == 't=':
 		# отправить в класс для работы. Класс должен вернуть ответ и выполнить всю работу
-		Finance_module.check_handler_QR(message.text, message.from_user, 1)
+		organisation_name, total_sum, DT, products = Finance_module.check_handler_QR(message.text, message.from_user, 1)
+		message_template = f'Ваш чек от {organisation_name}, был послан {DT}:\n'
+		for elem in products:
+			message_template += f'{elem.name} - {elem.sum//100} руб.\n'
+		bot.reply_to(message, message_template)
+
 
 @bot.message_handler(content_types=['document', 'photo'])
 def QR_code_handler(message):
@@ -36,7 +47,7 @@ def QR_code_handler(message):
 	downloaded_file = bot.download_file(file_info.file_path)
 	with open(f'QR_buffer/{file_name}', 'wb') as new_file:
 		new_file.write(downloaded_file)
-	bot.reply_to('Приветик')
 
+	bot.reply_to(message, 'Приветик')
 
 bot.polling(none_stop=True, timeout=123)
